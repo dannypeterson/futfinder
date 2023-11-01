2dceb67a89338895b5742014001dc17b0f50aecc
Danny <dannypeterson28@gmail.com>
2023-11-01 17:13:34 -0400

    first react commit

---
+++ frontend/src/components/Player.jsx
@@ -0,0 +1,13 @@
+// initally player is a silouette, game logic checks if the guess is correct after every guess. If it is correct, then render the player image by calling api
+
+const Player = () => {
+    return(
+        <>
+            <div>
+                Player will go here
+            </div>
+        </>
+    )
+}
+
+export default Player
