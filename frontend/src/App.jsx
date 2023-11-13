9710e3e6dfd71bba6fbbb5dc228f60451ce6e348
Danny <dannypeterson28@gmail.com>
2023-11-13 18:36:32 -0500

    added guess funcitonality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,32 +5,49 @@ import UserSearchBox from './components/UserSearchBox'
 import axios from 'axios'
 
 function App() {
-
   const [playerData, setPlayerData] = useState(null)
+  const [remainingAttempts, setRemainingAttempts] = useState(3)
+  const [userGuess, setUserGuess] = useState('')
+  const [gameOver, setGameOver] = useState(false)
+  const [isCorrect, setIsCorrect] = useState(false)
 
   const get_random_player = async () => {
     try {
-      const response = await axios.get('http://127.0.0.1:8000/player/',)
-      console.log(response.data[0])
+      const response = await axios.get('http://127.0.0.1:8000/player/')
       setPlayerData(response.data[0])
-  } catch(error) {
+    } catch (error) {
       console.log(error)
+    }
   }
+
+  const handleGuess = () => {
+    if (userGuess != playerData.name && remainingAttempts > 0) {
+      setRemainingAttempts(remainingAttempts - 1)
+    } else {
+      console.log('Player revealed')
+      setGameOver(true)
+    }
   }
 
   useEffect(() => {
     get_random_player()
   }, [])
 
-  const submitGuess = (guess) => {
-    // post guess to url
-  }
-
-
   return (
     <>
-    {playerData && <PlayerCard playerData = {playerData}/>}
-    <UserSearchBox />
+      {playerData && <PlayerCard playerData={playerData} remainingAttempts={remainingAttempts} gameOver={gameOver} />}
+      <div className="user-input">
+        <input
+          type="text"
+          placeholder="Enter a player's name here"
+          value={userGuess}
+          onChange={(e) => setUserGuess(e.target.value)}
+        />
+        {playerData && <button onClick={handleGuess} type="button">
+          Enter
+        </button>}
+      </div>
+      <p>Remaining Attempts: {remainingAttempts}</p>
     </>
   )
 }
--- frontend/src/components/PlayerCard.jsx
+++ frontend/src/components/PlayerCard.jsx
@@ -1,73 +1,103 @@
 import { useEffect, useState } from 'react'
 import fifaCard from '../assets/fifa-card.png'
 import axios from 'axios'
+import App from '../App'
 
-const PlayerCard = ({userStrikes, playerData, playerImage}) => {
-    userStrikes = 2
-    playerImage = 'https://selimdoyranli.com/cdn/fut-player-card/img/messi.png'
+const PlayerCard = ({ remainingAttempts, playerData, playerImage, gameOver }) => {
+  playerImage = 'https://selimdoyranli.com/cdn/fut-player-card/img/messi.png'
 
-    return(
-        <>
-        <div className="fut-player-card">
-            <div className="player-card-top">
-                <div className="player-master-info">
-                    <div className="player-rating">
-                        <span>{playerData.rating}</span>
-                    </div>
-                    <div className="player-position">
-                        <span>{playerData.position}</span>
-                    </div>
-                    <div className="player-nation">
-                        <img src={playerData.nationality} alt="player nation" draggable="false"/>
-                    </div>
-                    <div className="player-club">
-                        <img src={playerData.club} alt="player club" draggable="false"/>
-                    </div>
-                </div>
-                <div className="player-picture">
-                    <img src={playerImage} alt="player img" draggable="false"/>
-                </div>
+  return (
+    <>
+      <div className="fut-player-card">
+        <div className="player-card-top">
+          <div className="player-master-info">
+            <div className="player-rating">
+              <span>{playerData.rating}</span>
             </div>
-            <div className="player-card-bottom">
-                <div className="player-info">
-                    <div className="player-name">
-                        <span>{playerData.name}</span>
-                    </div>
-                    <div className="player-features">
-                        <div className="player-features-col">
-                            <span>
-                                <div className="player-feature-value">{playerData.pace}</div>
-                                <div className="player-feature-title">PAC</div>
-                            </span>
-                            <span>
-                                <div className="player-feature-value">{playerData.shooting}</div>
-                                <div className="player-feature-title">SHO</div>
-                            </span>
-                            <span>
-                                <div className="player-feature-value">{playerData.passing}</div>
-                                <div className="player-feature-title">PAS</div>
-                            </span>
-                        </div>
-                        <div className="player-features-col">
-                            <span>
-                                <div className="player-feature-value">{playerData.dribbling}</div>
-                                <div className="player-feature-title">DRI</div>
-                            </span>
-                            <span>
-                                <div className="player-feature-value">{playerData.defending}</div>
-                                <div className="player-feature-title">DEF</div>
-                            </span>
-                            <span>
-                                <div className="player-feature-value">{playerData.physicality}</div>
-                                <div className="player-feature-title">PHY</div>
-                            </span>
-                        </div>
-                    </div>
-                </div>
+            <div className="player-position">
+              {remainingAttempts <= 2 ? (
+                <span>{playerData.position}</span>
+              ) : (
+                <span>?</span>
+              )}
             </div>
+            <div className="player-nation">
+              {remainingAttempts <= 1 ? (
+                <img
+                  src={playerData.nationality}
+                  alt="nation"
+                  draggable="false"
+                />
+              ) : null}
+            </div>
+            <div className="player-club">
+              {remainingAttempts == 0 ? (
+                <img src={playerData.club} alt="club" draggable="false" />
+              ) : null}
+            </div>
+          </div>
+          <div className="player-picture">
+            {gameOver ? (
+              <img src={playerImage} alt="player img" draggable="false" />
+            ) : (
+              <img src={playerImage} style={{ filter: 'brightness(0)' }} alt="silouette" draggable="false" />
+            )}
+          </div>
         </div>
-        </>
-    )
+        <div className="player-card-bottom">
+          <div className="player-info">
+            <div className="player-name">
+              {gameOver ? (
+                <span>{playerData.name}</span>
+              ) : (
+                <span style={{ opacity: 0 }}>UNKNOWN PLAYER</span>
+              )}
+            </div>
+            <div className="player-features">
+              <div className="player-features-col">
+                <span>
+                  <div className="player-feature-value">{playerData.pace}</div>
+                  <div className="player-feature-title">PAC</div>
+                </span>
+                <span>
+                  <div className="player-feature-value">
+                    {playerData.shooting}
+                  </div>
+                  <div className="player-feature-title">SHO</div>
+                </span>
+                <span>
+                  <div className="player-feature-value">
+                    {playerData.passing}
+                  </div>
+                  <div className="player-feature-title">PAS</div>
+                </span>
+              </div>
+              <div className="player-features-col">
+                <span>
+                  <div className="player-feature-value">
+                    {playerData.dribbling}
+                  </div>
+                  <div className="player-feature-title">DRI</div>
+                </span>
+                <span>
+                  <div className="player-feature-value">
+                    {playerData.defending}
+                  </div>
+                  <div className="player-feature-title">DEF</div>
+                </span>
+                <span>
+                  <div className="player-feature-value">
+                    {playerData.physicality}
+                  </div>
+                  <div className="player-feature-title">PHY</div>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default PlayerCard
