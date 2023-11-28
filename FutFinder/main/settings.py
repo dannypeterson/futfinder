eb7fa4fa3650abbc942f6cd27777e642d3027641
Danny <dannypeterson28@gmail.com>
2023-11-27 19:18:50 -0500

    more prep for deployment

--- FutFinder/main/settings.py
+++ FutFinder/main/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = []
 
