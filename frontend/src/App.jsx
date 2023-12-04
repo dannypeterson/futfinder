77a3c4e44a336c6ffff9881be6ee840684b74a2e
Danny <dannypeterson28@gmail.com>
2023-12-04 18:39:16 -0500

    consolidated fetching images into one function

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,70 +11,42 @@ function App() {
   const [playerNation, setPlayerNation] = useState(null)
   const [playerClub, setPlayerClub] = useState(null)
 
+  const [loading, setLoading] = useState(true)
+
   const [showHowToPlay, setShowHowToPlay] = useState(true)
 
-  // axios.defaults.baseURL = 'http://localhost:8000/';
-  // axios.defaults.baseURL = 'https://futfinder-d2b9385f217b.herokuapp.com/';
-
   const baseURL = import.meta.env.VITE_BASE_URL
+
+  const fetchImage = async (url) => {
+    const imageResponse = await axios.get(url, {
+      headers: {
+        accept: 'image/png',
+        'X-AUTH-TOKEN': import.meta.env.VITE_FUTDB_KEY,
+      },
+      responseType: 'arraybuffer',
+    });
+    const imageBlob = new Blob([imageResponse.data], { type: 'image/png' });
+    return URL.createObjectURL(imageBlob);
+  };
 
 
   const getPlayerFromDB = async () => {
     // this func should only run once every 24hr, so need to cache it and check cache before running this func
     try {
-      // player data
+      setLoading(true)
       const response = await axios.get(`${baseURL}/api/player/`)
-      setPlayerData(response.data[0])
       const futdbID = response.data[0].futdb_id
       const nationID = response.data[0].nationality.futdb_id
       const clubID = response.data[0].club.futdb_id
 
-      // player image
-      const imageResponse = await axios.get(
-        `https://futdb.app/api/players/${futdbID}/image`,
-        {
-          headers: {
-            accept: 'image/png',
-            'X-AUTH-TOKEN': import.meta.env.VITE_FUTDB_KEY
-          },
-          responseType: 'arraybuffer'
-        }
-      )
-      const imageBlob = new Blob([imageResponse.data], { type: 'image/png' })
-      const imageUrl = URL.createObjectURL(imageBlob)
-      setPlayerImage(imageUrl)
-
-      // nation image
-      const nationResponse = await axios.get(
-        `https://futdb.app/api/nations/${nationID}/image`,
-        {
-          headers: {
-            accept: 'image/png',
-            'X-AUTH-TOKEN': import.meta.env.VITE_FUTDB_KEY
-          },
-          responseType: 'arraybuffer'
-        }
-      )
-      const nationBlob = new Blob([nationResponse.data], { type: 'image/png' })
-      const nationUrl = URL.createObjectURL(nationBlob)
-      setPlayerNation(nationUrl)
-
-      // club image
-      const clubResponse = await axios.get(
-        `https://futdb.app/api/clubs/${clubID}/image`,
-        {
-          headers: {
-            accept: 'image/png',
-            'X-AUTH-TOKEN': import.meta.env.VITE_FUTDB_KEY
-          },
-          responseType: 'arraybuffer'
-        }
-      )
-      const clubBlob = new Blob([clubResponse.data], { type: 'image/png' })
-      const clubUrl = URL.createObjectURL(clubBlob)
-      setPlayerClub(clubUrl)
+      setPlayerData(response.data[0])
+      setPlayerImage(await fetchImage(`https://futdb.app/api/players/${futdbID}/image`))
+      setPlayerNation(await fetchImage(`https://futdb.app/api/nations/${nationID}/image`))
+      setPlayerClub(await fetchImage(`https://futdb.app/api/clubs/${clubID}/image`))
     } catch (error) {
       console.log(error)
+    } finally {
+      setLoading(false)
     }
   }
 
